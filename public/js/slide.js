214d953985eea56e6ec0bef6086b84e8ed3d09a3
Peter Ade-Ojo <peteradeojo@outlook.com>
2021-11-26 14:16:27 +0100

    product gallery scrolling done

--- public/js/slide.js
+++ public/js/slide.js
@@ -1,33 +1,15 @@
 // @ts-check
 
 document.addEventListener('DOMContentLoaded', () => {
-	document.querySelectorAll('.slider').forEach((slideshow) => {
-		// console.log(slideshow);
-		let slideIndex = 1;
-
-		const showSlides = (n) => {
-			let i;
-			const slides = slideshow.querySelectorAll('.slide');
-			if (n > slides.length) slideIndex = 1;
-			if (n < 1) slideIndex = slides.length;
-
-			for (i = 0; i < slides.length; i++) {
-				slides[i].style.display = 'none';
-			}
-
-			slides[slideIndex - 1].style.display = 'initial';
-		};
-
-		showSlides(slideIndex);
-		const nextButton = slideshow.querySelector('button.next');
-		const prevButton = slideshow.querySelector('button.previous');
-		let index = 0;
-
-		nextButton.addEventListener('click', () => {
-			showSlides(slideIndex += 1);
+	const gallery = document.querySelector('.gallery');
+	gallery.querySelector('button.next').addEventListener('click', (e) => {
+		gallery.querySelector('.scroller').scrollBy({
+			left: 200,
 		});
-		prevButton.addEventListener('click', () => {
-			showSlides(slideIndex -= 1);
+	});
+	gallery.querySelector('button.previous').addEventListener('click', (e) => {
+		gallery.querySelector('.scroller').scrollBy({
+			left: -200,
 		});
 	});
 });
