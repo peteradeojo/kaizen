094904dfa19d4e6391bba846832ed1052e373448
Peter Ade-Ojo <peteradeojo@outlook.com>
2022-02-15 09:06:38 +0100

    frontend rebuild with api

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,11 @@
 $(() => {
-	// console.log(RES_HOST);
 	const displayItems = (obj) => {
 		console.log(obj);
 	};
 
 	const loadItems = async () => {
 		try {
-			const items = await fetch(`${RES_HOST}/products`);
+			const items = await fetch(`${RES_HOST}/api/products`);
 			displayItems(await items.json());
 		} catch (error) {
 			console.error(error);
